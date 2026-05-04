7399cc519ac41d993d95a619edfe366f69921c7f
LanicTT <mail@nico-lange.de>
2026-05-04 12:08:29 +0200

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from www.nico-lange.de"; //letter subject
+	$to = 'mail@nico-lange.de'; // Enter Your E-mail
+	$from = 'mail@nico-lange.de'; // Admin e-mail
+	$fromName = 'Nico Lange - Webiste'; // Your company name
+	$charset = 'UTF-8';
+?>
